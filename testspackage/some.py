0ac71624fd79d4c8003c2d538057cf5269d96c9c
donutkey <d0nutk3y@gmail.com>
2022-09-14 15:10:00 +0300

    readme changes part 3

--- testspackage/some.py
+++ testspackage/some.py
@@ -1,10 +1,19 @@
-def do_something():
-    a = 'somewhat to know'
-    b = 'somehow to learn'
-    return a + ' ' + b + '!'
+def get_out_line(ip_to_find:int):
+    result = -1
+    for x in global_table:
+        ip = x[0]
+        netmask = x[1]
+        out_line = x[2]
+        if (ip==ip_to_find):
+            return out_line
+    return result
 
+global_table = [(1,111,11),(2,222,22),(3,333,33)]
 
 if __name__ == '__main__':
-    result = do_something()
-    print(result)
-    print('update v1')
+    print('begining')
+    for i in range(0,7):
+        out_line = get_out_line(i)
+        print('ip is: ' + str(i))
+        print('outline found: ' + str(out_line))
+
